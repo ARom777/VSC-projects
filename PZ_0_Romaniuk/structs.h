ea969ac07a4aaae83fb785f213fca861ce97e680
Artem Romanuck <aaromanyuk@mai.education>
2025-09-27 22:37:35 +0300

    Обновил файл structs.h, прописав туда все возможные структуры

---
+++ PZ_0_Romaniuk/structs.h
@@ -0,0 +1,20 @@
+struct Point 
+{
+    double x, y;
+};
+
+struct Circle
+{
+    double radius;
+    double lengthOfCircle;
+    double areaOfCircle;
+    Point center;
+};
+
+struct Square
+{
+    double lengthOfSquareSide;
+    Point upperLeftCorner;
+    double perimeter;
+    double area;
+};
