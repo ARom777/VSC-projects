d7c0aa56dfdc2b6b1f507b7322746e8d2df41d71
Artem Romanuck <aaromanyuk@mai.education>
2025-09-29 01:27:44 +0300

    Сделал func.h и заготовку func.cpp

--- PZ_0_Romaniuk/func.cpp
+++ PZ_0_Romaniuk/func.cpp
@@ -1,1 +1,101 @@
 #include "structs.h"
+#include "func.h"
+
+void inputPointCoordinates(Point &p)
+{
+}
+
+void outputPointCoordinates(const Point &p)
+{
+}
+
+void readCircle(Circle &c)
+{
+}
+
+void writeCircle(const Circle &c)
+{
+}
+
+double lengthCircle(double radius)
+{
+    return 0.0;
+}
+
+double areaCircle(double radius)
+{
+    return 0.0;
+}
+
+void readSquare(Square &s)
+{
+}
+
+void writeSquare(const Square &s)
+{
+}
+
+double perimeterSquare(double lengthSide)
+{
+    return 0.0;
+}
+
+double areaSquare(double lengthSide)
+{
+    return 0.0;
+}
+
+bool pointInCicrle(const Point &p, const Circle &c)
+{
+    return false;
+}
+
+bool pointInSquare(const Point &p, const Square &s)
+{
+    return false;
+}
+
+bool pointOnCicrle(const Point &p, const Circle &c)
+{
+    return false;
+}
+
+bool pointOnSquare(const Point &p, const Square &s)
+{
+    return false;
+}
+
+bool intersectionOfCircles(const Circle &c1, const Circle &c2)
+{
+    return false;
+}
+
+bool intersectionOfSquares(const Square &s1, const Square &s2)
+{
+    return false;
+}
+
+bool intersectionOfCicrleSquare(const Circle &c, const Square &s)
+{
+    return false;
+}
+
+bool belongingCircles(const Circle &inner, const Circle &outer)
+{
+    return false;
+}
+
+bool belongingSquares(const Square &inner, const Square &outer)
+{
+    return false;
+}
+
+bool belongingSquareToCircle(const Square &sInner, const Circle &cOuter)
+{
+    return false;
+}
+
+bool belongingCicrleToSquare(const Circle &cInner, const Square &sOuter)
+{
+    return false;
+}
--- PZ_0_Romaniuk/func.h
+++ PZ_0_Romaniuk/func.h
@@ -2,4 +2,32 @@
 #define FUNC_H
 #include "structs.h"
 
+void inputPointCoordinates(Point &p);
+void outputPointCoordinates(const Point &p);
+
+void readCircle(Circle &c);
+void writeCircle(const Circle &c);
+double lengthCircle(double radius);
+double areaCircle(double radius);
+
+void readSquare(Square &s);
+void writeSquare(const Square &s);
+double perimeterSquare(double lengthSide);
+double areaSquare(double lengthSide);
+
+bool pointInCicrle(const Point &p, const Circle &c);
+bool pointInSquare(const Point &p, const Square &s);
+
+bool pointOnCicrle(const Point &p, const Circle &c);
+bool pointOnSquare(const Point &p, const Square &s);
+
+bool intersectionOfCircles(const Circle &c1, const Circle &c2);
+bool intersectionOfSquares(const Square &s1, const Square &s2);
+bool intersectionOfCicrleSquare(const Circle &c, const Square &s);
+
+bool belongingCircles(const Circle &inner, const Circle &outer);
+bool belongingSquares(const Square &inner, const Square &outer);
+bool belongingSquareToCircle(const Square &sInner, const Circle &cOuter);
+bool belongingCicrleToSquare(const Circle &cInner, const Square &sOuter);
+
 #endif
